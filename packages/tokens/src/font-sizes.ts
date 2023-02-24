a5a7af240a7ea2d5fb501f7f2f1e68deb94140e1
JVMPRDEV <jvmpr.dev@gmail.com>
2023-02-23 23:39:27 -0300

    Feature - Colors Documment

---
+++ packages/tokens/src/font-sizes.ts
@@ -0,0 +1,14 @@
+export const fontSizes = {
+    xxs: '0.625rem',
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '4xl': '2.25rem',
+    '5xl': '3.5rem',
+    '6xl': '4rem',
+    '7xl': '4.5rem',
+    '8xl': '6rem',
+}
